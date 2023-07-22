91bc212daf0b68f4f8b023a93027a4879311ddc3
artificialnouveau <46301511+artificialnouveau@users.noreply.github.com>
2023-07-22 11:38:19 +0200

    Update replace_black_bar.py

--- replace_black_bar.py
+++ replace_black_bar.py
@@ -4,15 +4,28 @@ from PIL import Image
 
 input_folder = './input/'
 output_folder = './black_bar_output/'
+person_folder = './person/'
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-for filename in os.listdir(input_folder):
-    img = cv2.imread(os.path.join(input_folder, filename))
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+def anonymize_black_bar(input_folder, output_folder, person_folder):
+    for filename in os.listdir(input_folder):
+        img = cv2.imread(os.path.join(input_folder, filename))
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+        person_files = glob.glob(person_folder + "*.jpg")
 
-    for (x, y, w, h) in faces:
-        img = cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 0), -1)
+        if len(person_files) > 0:
+            for (x, y, w, h) in faces:
+                face = gray[y:y+h, x:x+w]
+                label, confidence = recognizer.predict(face)
+                if label == 0:
+                    img[y:y+h, x:x+w] = 0
+        else:
+            for (x, y, w, h) in faces:
+                img[y:y+h, x:x+w] = 0
 
-    cv2.imwrite(os.path.join(output_folder, filename), img)
+        cv2.imwrite(os.path.join(output_folder, filename), img)
+
+
+anonymize_black_bar(input_folder, output_folder, person_folder)
